ab6099b8846f0a5e4639b46e27f8f14436fb2cfa
Graham Bates <info@grahambates.com>
2023-05-14 14:09:33 +0100

    WIP

--- bee.lua
+++ bee.lua
@@ -1,6 +1,7 @@
 --{
 TIC=function()
   --{
+  -- sfx(0)
   cls(-1)
   for z=120,0,-1 do
     poke(z%48+16320,240/(1+2^(5-z%48/5-z%48*2%3)))
@@ -9,14 +10,14 @@ TIC=function()
       tri(
       		-- top
         120+Z*(x+math.sin(t/20+x)/2+math.sin(t+x+z)),
-        20+Z*(math.sin((t+x+z)/40)+math.sin(t/40)+7),
+        20+Z*(math.sin((t+z)/25)+math.sin(x/10+t/100)*2+7),
         -- bottom-right
         120+Z*(x+.3+math.sin(t+x+z)),
-        20+Z*(math.sin((t+x+z)/40)+math.sin(t/40)+10),
+        20+Z*(math.sin((t+z)/25)+math.sin(x/10+t/100)*2+10),
         -- bottom-left
         120+Z*(x-.3+math.sin(t+x+z)),
-        20+Z*(math.sin((t+x+z)/40)+math.sin(t/40)+10),
-        z/6-5)
+        20+Z*(math.sin((t+z)/25)+math.sin(x/10+t/100)*2+10),
+        z/7-2)
     end
   end
   t=1+t
@@ -25,14 +26,14 @@ end
 t=0
 OVR=function()
   cls()
-  circ(120,5,30,12)
+  circ(120+math.sin(t/1000)*20,5,30,12)
   circ(120+math.sin(t/100)*20,math.sin(t/70)*10+80,3,15)
   circ(120+math.sin(t/100)*20,math.sin(t/70)*10+84,5,4)
   circ(120+math.sin(t/100)*20,math.sin(t/70)*10+84,4,15)
   circ(120+math.sin(t/100)*20,math.sin(t/70)*10+85,3,4)
   circ(120+math.sin(t/100)*20,math.sin(t/70)*10+86,2,15)
-  elli(113+math.sin(t/100)*20,math.sin(t/70)*10+t%3*80,6,4,12)
-  elli(127+math.sin(t/100)*20,math.sin(t/70)*10+t%3*80,6,4,12)
+  elli(113+math.sin(t/100)*20,math.sin(t/70)*10+t%3*80+t%2,6,4,12)
+  elli(127+math.sin(t/100)*20,math.sin(t/70)*10+t%3*80+t%2,6,4,12)
 end
 --}
 -- <PALETTE>
