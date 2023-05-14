f0df215a9ae272944b64e79fa2958baf71c32418
Graham Bates <info@grahambates.com>
2023-05-14 03:10:30 +0100

    WIP

--- bee.lua
+++ bee.lua
@@ -2,21 +2,21 @@
 TIC=function()
   --{
   cls(-1)
-  for z=140,0,-1 do
+  for z=120,0,-1 do
     poke(z%48+16320,240/(1+2^(5-z%48/5-z%48*2%3)))
-    Z=z-t%2/2
-    for x=15,-15,-1 do
+    Z=(z+20)/1000
+    for x=20,-20,-1 do
       tri(
       		-- top
-        (x+math.sin(x+t/20)/2+math.sin(x+t//2+z))*1000/Z+120,
-        (5-Z/20-math.sin((z+t//2)/20)-math.sin((t/2)/20))*1000/Z+70,
+        120+(x+math.sin(t/20+x)/2+math.sin(t+x+z))/Z,
+        20+(7+math.sin((t+x+z)/40)+math.sin(t/40))/Z,
         -- bottom-right
-        (x+.3+math.sin(x+t//2+z))*1000/Z+120,
-        (8-Z/20-math.sin((z+t//2)/20)-math.sin((t/2)/20))*1000/Z+70,
+        120+(x+.3+math.sin(t+x+z))/Z,
+        20+(10+math.sin((t+x+z)/40)+math.sin(t/40))/Z,
         -- bottom-left
-        (x-.3+math.sin(x+t//2+z))*1000/Z+120,
-        (8-Z/20-math.sin((z+t//2)/20)-math.sin((t/2)/20))*1000/Z+70,
-        z/7-5)
+        120+(x-.3+math.sin(t+x+z))/Z,
+        20+(10+math.sin((t+x+z)/40)+math.sin(t/40))/Z,
+        z/6-5)
     end
   end
   t=1+t
@@ -25,7 +25,7 @@ end
 t=0
 OVR=function()
   cls()
-  circ(120,10,30,12)
+  circ(120,5,30,12)
   circ(120+math.sin(t/100)*20,math.sin(t/70)*10+80,3,15)
   circ(120+math.sin(t/100)*20,math.sin(t/70)*10+84,5,4)
   circ(120+math.sin(t/100)*20,math.sin(t/70)*10+84,4,15)
